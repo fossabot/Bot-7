b2a51e971c2be2c7f5b18bcb5e2a6241f3068ad6
Jeongheegenius <jeonghee070911@naver.com>
2021-07-23 11:49:00 +0900

    ssh test

--- cogs/Core.py
+++ cogs/Core.py
@@ -6,7 +6,7 @@ class Core(commands.Cog, name="일반"):
         self.bot = bot
     @commands.command(name="핑", help="봇의 속도와 클라이언트 핑을 알려줘요!", aliases=["ping"])
     async def ping(self, ctx):
-        await ctx.reply(f'Pong! - {round(self.bot.latency * 1000)}ms')
+        await ctx.reply(f'퐁! - {round(self.bot.latency * 1000)}ms')
 
     @commands.command(name="개발자", help="민트초코 봇의 개발자를 알려줘요!", aliases=["hellothisisverification"])
     async def hellothisisverification(self, ctx):
