a50b0eab4b51709d77ea07e1d7c19126a78dc5b4
jeongheegenius <jeonghee070911@naver.com>
2021-08-06 08:59:19 +0900

    many command add

---
+++ cogs/mods.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import typing
+from pytz import timezone, utc
+import datetime
+import asyncio
+
+class mods(commands.Cog, name="관리"):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command(name="kick", help="상대를 서버 밖으로 내보내는 명령어", aliases=["추방"])
+    @commands.has_permissions(kick_members=True)
+    @commands.bot_has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason="사유가 지정되지 않음"):
+        await member.kick(reason=reason)
+        await ctx.reply(f'{member}님은 **{reason}**라는 이유로 서버에서 추방되었습니다.', mention_author=False)
+        
+    @commands.command(name="ban", help="상대를 서버 밖으로 영원히 내쫓는 명령어", aliases=["차단"])
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason="사유가 지정되지 않음"):
+      await member.ban(reason=reason)
+      await ctx.reply(f'{member}님은 **{reason}**라는 이유로 서버에서 차단되셨습니다.', mention_author=False)
+
+def setup(bot):
+    bot.add_cog(mods(bot))
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord, asyncio, os
 from discord.ext import commands
+import discord.ext.tasks
 from jishaku.cog import Jishaku
 
-bot = commands.Bot(command_prefix='민초야 ', intents=discord.Intents.all())
+owners = [673471748599054336, 338902243476635650, 551212082645827594, 694545037014597664, 694027673088819210]
+
+bot = commands.Bot(command_prefix='민초야 ', owner_ids = set(owners), intents=discord.Intents.all())
 TOKEN = "ODY0NjgzMTU0NjQ3ODEwMDg5.YO5A8Q._EOhOu7XhK2fKJvEtk4YsG45JUA"
 
 for file in os.listdir("cogs"):
@@ -12,10 +15,18 @@ for file in os.listdir("cogs"):
 
 bot.load_extension('jishaku')
 
-bot.help_command=None
+#bot.help_command=None
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} 준비완료!")
+    @discord.ext.tasks.loop(seconds=5)
+    async def changing():
+        a = 0
+        b = 0
+        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f'{bot.command_prefix} help를 입력해 보아요!'))
+        
+    changing.start()
 
 bot.run(TOKEN)
