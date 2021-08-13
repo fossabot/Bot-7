ce2c8e6f0b34c33118e6b8286a025dff6b9d1606
jeongheegenius <jeonghee070911@naver.com>
2021-08-13 11:22:28 +0900

    개같은 env

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import os
 from discord.ext import commands
 import discord.ext.tasks
 from jishaku.cog import Jishaku
+import dotenv
+from dotenv import *
 
-owners = [673471748599054336, 338902243476635650, 551212082645827594, 694545037014597664, 694027673088819210]
+owners = [673471748599054336, 338902243476635650]
+
+config = dotenv_values(".env")
+token = config["token"]
 
 bot = commands.Bot(command_prefix='민초야 ', owner_ids = set(owners), intents=discord.Intents.all())
-TOKEN = "ODY0NjgzMTU0NjQ3ODEwMDg5.YO5A8Q._EOhOu7XhK2fKJvEtk4YsG45JUA"
 
 for file in os.listdir("cogs"):
     if file.endswith(".py"):
@@ -21,4 +25,4 @@ async def on_ready():
     print(f"{bot.user.name} 준비완료!")
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f'{bot.command_prefix} help를 입력해 보아요!'))
 
-bot.run(TOKEN)
+bot.run(token)
